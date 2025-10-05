e640ad17e2d3d9fbe22dd664a19eb1ad141a1c30
zulm23 <surat.zulm@gmail.com>
2025-10-05 12:05:04 +0700

    flutter add

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1252687960"
+    serviceWorkerVersion: "2679115253"
   }
 });
